eaf5ba14d1f512caced8743f462af0375e93f48f
Jose-Pedro-Bernardes <josepedro.bernardes1@gmail.com>
2026-02-05 22:15:26 -0300

    chore: setup inicial do projeto
    - Adicionadas stacks principais (Next.js, TypeScript, MUI, Axios, TanStack Query, React Hook Form + Zod)
    - Configurado favicon e ícone do app
    - Atualizado README com informações do projeto e instruções iniciais
    - Criada página inicial com mensagem 'Pronto para começar'

---
+++ README.md
@@ -0,0 +1,26 @@
+# OrganizaDaí
+
+**OrganizaDaí** é um sistema web de gestão pensado para **prestadores de serviço** que querem organizar seus clientes, serviços, agenda e finanças em um único lugar.  
+O objetivo é criar uma plataforma **prática, escalável e fácil de usar**, construída com **Next.js**, **TypeScript** e stacks modernas de front-end.
+
+Este projeto ainda está em desenvolvimento, mas já possui a estrutura base para começar a criar funcionalidades completas de um sistema FullStack.
+
+---
+
+## 🎯 Objetivo do projeto
+
+- Centralizar o gerenciamento de clientes, serviços e atendimentos  
+- Controlar agendamentos e compromissos de forma organizada  
+- Monitorar finanças e pagamentos  
+- Criar uma experiência moderna e responsiva usando **MUI**  
+- Permitir integração futura com backend via **Axios** e gerenciamento de estado com **TanStack Query**
+
+---
+
+## 🚀 Início rápido
+
+### 1. Clonar o repositório
+
+```bash
+git clone git@github.com:SEU_USUARIO/organizadai-web.git
+cd organizadai-web
---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "OrganizaDaí",
+  description: "OrganizaDai é um sistema web de gestão para prestadores de serviço, criado para centralizar clientes, serviços, agendamentos e pagamentos em um único lugar. O objetivo é simplificar a rotina e dar clareza financeira para profissionais autônomos, com uma interface moderna, rápida e fácil de usar.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <head>
+        <link rel="icon" type="image/png"  href="/icon.png" />
+      </head>
+
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
