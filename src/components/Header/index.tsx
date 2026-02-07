5ba1e42f45150ac936c0b6dff8fbadc0b68590bf
Jose-Pedro-Bernardes <josepedro.bernardes1@gmail.com>
2026-02-07 19:58:10 -0300

    refactor(header): usa Link do Next na logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,21 @@
 import Image from 'next/image'
 import styles from './Header.module.css'
+import Link from 'next/link'
 
 export default function Header() {
   return (
     <header className={styles.header}>
+      <Link href="/">
         <figure className={styles.logoContainer}>
             <Image
                 className={styles.logo}
                 src="/logotipo.png"
                 alt="Logo da Empresa"
-                width="480"
-                height="143"
+                width="400"
+                height="110"
             />
         </figure>
+      </Link>
     </header>
   )
 }
