929e9c481ad0a74c029b91e5846600664fb72d13
Jose-Pedro-Bernardes <josepedro.bernardes1@gmail.com>
2026-02-14 23:58:34 -0300

    Feat(Header): Adicionando lógica ao botão menu hamburguer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -56,6 +56,32 @@ export default function Header() {
           />
         </Link> 
       </div>
+      <ul className={styles.MenuNavMobile}>
+          <li><Link href="/sobre" className={styles.linkNav}>Sobre nós</Link></li>
+          <li><Link href="/solucoes" className={styles.linkNav}>Soluções</Link></li>
+          <li><Link href="/precos" className={styles.linkNav}>Preços</Link></li>
+          <li><Link href="/suporte" className={styles.linkNav}>Suporte</Link></li>
+          <Link href="/entrar" className={styles.login}>
+            <Image
+                className={styles.loginImage}
+                src="/loginImage.png"
+                alt="login"
+                width="341"
+                height="339"
+            />
+            <span className={styles.loginText}>Fazer Login</span>
+        </Link>
+        <Link href="cadastro" className={styles.cadastro}>
+            <span>Criar conta grátis</span>
+            <Image
+              className={styles.setaCadastro}
+              src="/setadireita.png"
+              alt="seta"
+              width="14"
+              height="14"
+            />
+        </Link> 
+      </ul>
     </header>
   )
 }
