3134714886cd35ccc5f27cfadaba986aeded66bf
Jose-Pedro-Bernardes <josepedro.bernardes1@gmail.com>
2026-03-03 23:44:05 -0300

    feat(em progresso): Removendo página de "Soluções" e trocando pela página de "Funcionalidades"

---
+++ src/app/funcionalidades/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./funcionalidades.module.css";
+export default function Funcionalidades() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Página Funcionalidades</h1>
+      </main>
+    </div>
+  );
+}
