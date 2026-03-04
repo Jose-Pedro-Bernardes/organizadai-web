ca0203dcd3f9b07d7895ae9e6c5229a5f33fb841
Jose-Pedro-Bernardes <josepedro.bernardes1@gmail.com>
2026-03-03 23:42:53 -0300

    feat(em progresso): Removendo página sobre e adicionando página "Para Quem É"

---
+++ src/app/paraqueme/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./paraqueme.module.css";
+export default function Paraqueme() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Página Para Quem É</h1>
+      </main>
+    </div>
+  );
+}
