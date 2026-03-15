8eb1b2ad2b44d293bc5137620ed867a08d8778b4
José Pedro Bernardes <josepedro.bernardes1@gmail.com>
2026-03-15 00:43:56 -0300

    chore(fix): Header.module.css to header.module.css

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from 'next/image'
-import styles from './Header.module.css'
+import styles from './header.module.css'
 import Link from 'next/link'
 import { useState } from "react";
 
